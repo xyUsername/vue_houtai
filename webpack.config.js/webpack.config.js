c4065481de60a51a4fbc866aedd5702609c7feb4
xuye <xy19930825@163.com>
2018-02-27 11:34:11 +0800

    vuehoutai

---
+++ webpack.config.js/webpack.config.js
@@ -0,0 +1,73 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry:'./src/main.js',
+    output:{
+        path:path.join(__dirname+'/dist'),
+        // publicPath: '/dist',
+        filename:'bundle.js'
+    },
+    module: {
+        rules: [
+          // 配置的是用来解析.css文件的loader(style-loader和css-loader)
+          {
+            // 1.0 用正则匹配当前访问的文件的后缀名是  .css
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'] //webpack底层调用这些包的顺序是从右到左
+          },
+          {
+            test: /\.less$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'less-loader'
+            }]
+        },
+        {
+          test: /\.scss$/,
+          use: [{
+              loader: 'style-loader'
+          }, {
+              loader: 'css-loader'
+          }, {
+              loader: 'sass-loader'
+          }]
+      },
+      {
+        test: /\.(png|jpg|gif)/,
+        use: [{
+            loader: 'url-loader',
+            options: {
+                limit: 50
+            }
+        }]
+    },{
+        test: /\.js$/,
+        // Webpack2建议尽量避免exclude，更倾向于使用include
+        // exclude: /(node_modules)/, // node_modules下面的.js文件会被排除
+        include: [path.resolve(__dirname, 'src')],
+        use: {
+          loader: 'babel-loader',
+          // options里面的东西可以放到.babelrc文件中去
+        //   options: {
+        //     presets: ['env']
+        //   }
+        }
+      },{
+        test: /\.vue$/,
+        loader: 'vue-loader'
+        }
+    
+        ]
+    
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'template.html',
+            title:'测试'
+        })
+    ]   
+}
